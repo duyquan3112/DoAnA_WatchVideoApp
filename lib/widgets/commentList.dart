3e4693e3a68aff10fd0dae9299ee4ac8c5a40a40
duyquan3112 <kiliva3112@gmail.com>
2023-04-14 12:51:25 +0700

    update comment upload

--- lib/widgets/commentList.dart
+++ lib/widgets/commentList.dart
@@ -16,75 +16,45 @@ class commentList extends StatefulWidget {
 }
 
 class _commentListState extends State<commentList> {
-  // List<commentModel> commentList = [];
-  // List<commentModel> getComment() {
-  //   var db = FirebaseFirestore.instance;
-  //   db.collection('comment_list').get().then((data) {
-  //     for (var value in data.docs) {
-  //       if (widget.vidId == value['vidId']) {
-  //         commentModel comments = new commentModel();
-  //         comments.avtUrl = value['avtUrl'];
-  //         comments.name = value['name'];
-  //         comments.content = value['content'];
-  //         comments.date = value['date'];
-  //         commentList.add(comments);
-  //       }
-  //     }
-  //   });
-
-  //   return commentList;
-  // }
-
   @override
   void initState() {
-    //getComment();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: StreamBuilder(
-          stream:
-              FirebaseFirestore.instance.collection('comment_list').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  shrinkWrap: false,
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    if (widget.vidId == snapshot.data!.docs[index]['vidId']) {
-                      return CommentChild(
-                        avtUrl: snapshot.data!.docs[index]['avtUrl'],
-                        content: snapshot.data!.docs[index]['content'],
-                        date: snapshot.data!.docs[index]['date'],
-                        name: snapshot.data!.docs[index]['name'],
-                      );
-                    }
-                    ;
-                  });
-            } else {
-              return Center(child: const Text('No data'));
+    CollectionReference comments =
+        FirebaseFirestore.instance.collection('comment_list');
+    return StreamBuilder<QuerySnapshot>(
+      stream: comments.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          Size size = MediaQuery.of(context).size;
+          return Center(
+            child: Transform.scale(
+              scale: 1,
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            if (document['vidId'] == widget.vidId) {
+              return CommentChild(
+                  avtUrl: document['avtUrl'],
+                  name: document['name'],
+                  content: document['content'],
+                  date: document['date']);
             }
-          }),
+            return const SizedBox(
+              height: 0,
+            );
+          }).toList(),
+        );
+      },
     );
   }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Container(
-  //       child: ListView.builder(
-  //           shrinkWrap: true,
-  //           physics: const AlwaysScrollableScrollPhysics(),
-  //           itemCount: commentList.length,
-  //           itemBuilder: (context, index) {
-  //             return CommentChild(
-  //               avtUrl: commentList[index].avtUrl!,
-  //               content: commentList[index].content!,
-  //               date: commentList[index].date!,
-  //               name: commentList[index].name!,
-  //             );
-  //           }));
-  // }
 }
