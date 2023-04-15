8ad18b9b9d4416de94e87a2019816d3048ca5f07
ThesngNguyen <ncthang151002@gmail.com>
2023-04-15 21:06:34 +0700

    Add Google SignIn Method

---
+++ lib/widgets/square_tile.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  const SquareTile({
+    super.key,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey[200],
+      ),
+      child: Row(
+        children:[ 
+          Image.asset(
+            imagePath,
+            height: 20,
+          ),
+          Text(
+            '  Sign in with Google'
+          ),
+        ],
+      ),
+    );
+  }
+}
