293dfcc9d0e8f788d60427c92aae61a42938769d
th50989 <th50989@gmail.com>
2023-04-25 14:01:31 +0700

    fixgrouptag

--- lib/pages/gamePage.dart
+++ lib/pages/gamePage.dart
@@ -17,41 +17,45 @@ class MyGameAppState extends State<MyGameApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('video_list').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Flexible(
-                  child: ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        infoVideo info = infoVideo();
-                        info.description =
-                            snapshot.data!.docs[index]['description'];
-                        info.title = snapshot.data!.docs[index]['title'];
-                        info.url = snapshot.data!.docs[index]['videoUrl'];
-                        info.vidId = snapshot.data!.docs[index].id;
-                        info.types = snapshot.data!.docs[index]['type'];
-                        if (info.types == 'games') {
-                          return videoCard(
-                            users: widget.users,
-                            // uRLVideo: snapshot.data!.docs[index]['videoUrl'],
-                            // title: snapshot.data!.docs[index]['title'],
-                            // des: snapshot.data!.docs[index]['description'],
-                            // vidId: snapshot.data!.docs[index].id,
-                            infoVid: info,
-                          );
-                        }
-                      }),
-                ),
-              ],
-            );
-          } else
-            return Text('No Game');
-        });
+    return (Scaffold(
+        appBar: AppBar(title: const Text('This is Game Page')),
+        body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('video_list').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Flexible(
+                      child: ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            infoVideo info = infoVideo();
+                            info.description =
+                                snapshot.data!.docs[index]['description'];
+                            info.title = snapshot.data!.docs[index]['title'];
+                            info.url = snapshot.data!.docs[index]['videoUrl'];
+                            info.vidId = snapshot.data!.docs[index].id;
+                            info.types = snapshot.data!.docs[index]['type'];
+                            if (info.types == 'games') {
+                              return videoCard(
+                                users: widget.users,
+                                // uRLVideo: snapshot.data!.docs[index]['videoUrl'],
+                                // title: snapshot.data!.docs[index]['title'],
+                                // des: snapshot.data!.docs[index]['description'],
+                                // vidId: snapshot.data!.docs[index].id,
+                                infoVid: info,
+                              );
+                            }
+                            return Container();
+                          }),
+                    ),
+                  ],
+                );
+              } else
+                return Text('No Game');
+            })));
   }
 }
--- lib/pages/musicPage.dart
+++ lib/pages/musicPage.dart
@@ -19,41 +19,47 @@ class MyMusicAppState extends State<MyMusicApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('video_list').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                Flexible(
-                  child: ListView.builder(
-                      physics: const BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        infoVideo info = infoVideo();
-                        info.description =
-                            snapshot.data!.docs[index]['description'];
-                        info.title = snapshot.data!.docs[index]['title'];
-                        info.url = snapshot.data!.docs[index]['videoUrl'];
-                        info.vidId = snapshot.data!.docs[index].id;
-                        info.types = snapshot.data!.docs[index]['type'];
-                        if (info.types == 'music') {
-                          return videoCard(
-                            // uRLVideo: snapshot.data!.docs[index]['videoUrl'],
-                            // title: snapshot.data!.docs[index]['title'],
-                            // des: snapshot.data!.docs[index]['description'],
-                            // vidId: snapshot.data!.docs[index].id,
-                            users: widget.users,
-                            infoVid: info,
-                          );
-                        }
-                      }),
-                ),
-              ],
-            );
-          } else
-            return Text('No music');
-        });
+    return (Scaffold(
+        appBar: AppBar(title: const Text('Music Page')),
+        body: StreamBuilder(
+            stream:
+                FirebaseFirestore.instance.collection('video_list').snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Flexible(
+                      child: ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            infoVideo info = infoVideo();
+                            info.description =
+                                snapshot.data!.docs[index]['description'];
+                            info.title = snapshot.data!.docs[index]['title'];
+                            info.url = snapshot.data!.docs[index]['videoUrl'];
+                            info.vidId = snapshot.data!.docs[index].id;
+                            info.types = snapshot.data!.docs[index]['type'];
+
+                            print(info.types);
+                            if (info.types == 'musics') {
+                              return videoCard(
+                                // uRLVideo: snapshot.data!.docs[index]['videoUrl'],
+                                // title: snapshot.data!.docs[index]['title'],
+                                // des: snapshot.data!.docs[index]['description'],
+                                // vidId: snapshot.data!.docs[index].id,
+                                users: widget.users,
+                                infoVid: info,
+                              );
+                            }
+                            return Container();
+                          }),
+                    ),
+                  ],
+                );
+              } else
+                return Text('No music');
+            })));
   }
 }
