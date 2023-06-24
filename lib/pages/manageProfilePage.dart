82df3437c7697e7775c999310056423c21153e92
ThesngNguyen <ncthang151002@gmail.com>
2023-06-24 23:05:45 +0700

    FixBug and add UI Manage Profile

--- lib/pages/manageProfilePage.dart
+++ lib/pages/manageProfilePage.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:do_an/pages/homePage.dart';
+import 'package:do_an/models/getUserData.dart';
+import 'package:do_an/widgets/drawerMenu.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:do_an/widgets/error_SnackBar.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:do_an/pages/personalProfilePage.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
 class manageProfilePage extends StatefulWidget {
@@ -10,10 +16,86 @@ class manageProfilePage extends StatefulWidget {
 }
 
 class _manageProfilePageState extends State<manageProfilePage> {
+  UserData? currentUser;
+  late int _selectedIndex;
+  String? userId;
+  bool _isLoggedIn = true;
+
+  @override
+  void initState() {
+    _selectedIndex = 0;
+    super.initState();
+    currentUser = UserData.getCurrentUser();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _handleLogout() async {
+    await FirebaseAuth.instance.signOut();
+    setState(() {
+      currentUser = null;
+      userId = null;
+      _isLoggedIn = false;
+      _selectedIndex = 0;
+    });
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyHomePage(
+        users: UserData(uid: ''),
+        userId: '',
+      )),
+    );
+  }
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
+    void gotoProfilePage() {
+      if (currentUser == true) {
+        // Hiển thị Flushbar nếu currentUser là null
+        errorSnackBar(
+          errMess: 'Bạn đang ở trang cá nhân!',
+        ).build(context);
+      } else {
+        // Điều hướng qua trang personalProfilePage nếu có currentUser
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) => personalProfilePage(
+                    currentUser: currentUser,
+                    isLogin: _isLoggedIn,
+                  )),
+        );
+      }
+    }
+
+    void gotoHomePage() {
+      if (currentUser == true) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MyHomePage(
+                    users: currentUser!,
+                    userId: userId!,
+                  )),
+        );
+      } else {
+        // Điều hướng qua trang personalProfilePage nếu không có currentUser
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => MyHomePage(
+                    users: UserData(uid: ''),
+                    userId: '',
+                  )),
+        );
+      }
+    }
+    
     return Scaffold(
       key: _scaffoldKey,
       body: Container(
@@ -34,7 +116,7 @@ class _manageProfilePageState extends State<manageProfilePage> {
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       SizedBox(
                         child: IconButton(
@@ -52,17 +134,14 @@ class _manageProfilePageState extends State<manageProfilePage> {
                           left: 0,
                         ),
                         child: const Text(
-                          "Home",
+                          "Manange Profile",
                           style: TextStyle(
                             color: Colors.white, 
                             fontSize: 25.0,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                      ),
-                      Icon(
-                        Icons.logout,
-                      )         
+                      ),        
                     ],
                   ),
                 ),
@@ -70,6 +149,12 @@ class _manageProfilePageState extends State<manageProfilePage> {
             ),
           ],
         ),
+      ),
+      drawer: drawerMenu(
+        userData: currentUser,
+        onHomePageTap: gotoHomePage,
+        onProfileTap: gotoProfilePage,
+        onSignOut: _handleLogout,
       ),
     );
   }
